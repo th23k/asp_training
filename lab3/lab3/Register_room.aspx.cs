c634512d00f39c02430edb68262240ca81d3d23b
th23k <fffv2003x@gmail.com>
2023-12-15 18:31:44 +0700

    create

---
+++ lab3/lab3/Register_room.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab3
+{
+    public partial class Register_room : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDangKy_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                lblThongBao.Text = string.Format($"Chúc mừng  {txtHoTen.Text} đăng ký thành công! <br/> Cơ quan: {txtCoQuan.Text} <br/> Email: {txtEmail.Text} <br/> ngày checkin: {txtCheckin.Text} <br/> Số ngày ở: {txtSoNgay.Text} <br/> Loại phòng: {cbxLoaiPhong.SelectedItem.Text}");
+            }
+
+        }
+    }
+}
